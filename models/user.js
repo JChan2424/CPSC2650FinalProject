f05ccdd64dc3be3fdf7ea6f8fff6306ed932c7ca
Ashwin <69374625+xxiz@users.noreply.github.com>
2023-07-25 13:27:50 -0700

    add: mongodb models

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const User = (username, password, firstName, lastName, role) => {
+    return {
+        Username: username,
+        Password: password,
+        FirstName: firstName,
+        LastName: lastName,
+        Role: role,
+    }
+}
+module.exports = User
