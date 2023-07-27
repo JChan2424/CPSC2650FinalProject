1d739da776640df117da24b5865aded36d7497d3
Ashwin <69374625+xxiz@users.noreply.github.com>
2023-07-27 16:19:35 -0700

    add: Announcement Controller #17 add & delete methods

--- controllers/announcementController.js
+++ controllers/announcementController.js
@@ -1,10 +1,30 @@
 // Imports
 const util = require("../models/util.js");
 const config = require("../server/config/config");
-// const announcement = require("../models/announcement"); 
+const Announcement = require("../models/announcement");
 const client = util.getMongoClient();
 const express = require("express");
 const announcementController = express.Router();
+
+const validateAnnoucement = (req, res, next) => {
+  if (req.body.topic == null) {
+    req.body.topic = "general";
+  }
+
+  if (req.body.message == null) {
+    res.status(400);
+    return res.send("Message is required");
+  }
+
+  if (req.body.author == null) {
+    res.status(400);
+    return res.send("Author is required");
+  }
+
+  req.body.date = new Date();
+
+  next();
+};
 
 // getAll
 announcementController.get(
@@ -60,3 +80,34 @@ announcementController.get(
   }
 );
 
+// addAnnouncement
+announcementController.post(
+  "/api/announcements",
+  validateAnnoucement,
+  util.logRequest,
+  async (req, res, next) => {
+    let collection = client.db().collection("Announcements");
+
+    let ann = new Announcement(
+      req.body.topic,
+      req.body.message,
+      req.body.author,
+      req.body.date
+    );
+
+    let result = await util.insertOne(collection, ann);
+    res.status(200).json(result);
+  }
+);
+
+// deleteAnnouncement
+announcementController.delete(
+  "/api/announcements/:id",
+  util.logRequest,
+  async (req, res, next) => {
+    let collection = client.db().collection("Announcements");
+
+    let result = await util.deleteOne(collection, { _id: req.params.id });
+    res.status(200).json(result);
+  }
+);
