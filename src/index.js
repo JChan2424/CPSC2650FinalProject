51db6e583afab806e1cb90ec69d2588e0db74651
Kieran <kieranlee88@gmail.com>
2023-07-29 05:47:52 -0700

    Fixed boostrap not working with react. installed boostrap with npm, updated webpack config, moved index.html to views.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+
 // import './style.css';
 import App from './App.js';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path'); 
 module.exports = { 
     mode:"development",
+    devtool:"source-map",
     entry: './src/index.js', 
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public') 
+        path: path.resolve(__dirname, 'views') 
     },
     module: { 
         rules: [
@@ -18,5 +19,9 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    watchOptions: {
+        poll: true,
+        ignored: /node_modules/
+      }
 };
