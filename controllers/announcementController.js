72389d65b56a57fb3363aac5e715643d068da136
Ashwin <69374625+xxiz@users.noreply.github.com>
2023-07-30 01:39:53 -0700

    fix: announcement routes (tested)

--- controllers/announcementController.js
+++ controllers/announcementController.js
@@ -2,12 +2,18 @@
 const util = require("../models/util.js");
 const Announcement = require("../models/announcement");
 const client = util.getMongoClient();
+const ObjectId = require("mongodb").ObjectId;
 const express = require("express");
 const announcementController = express.Router();
 
+// Custom Middleware
+
 const validateAnnoucement = (req, res, next) => {
+
   if (req.body.topic == null) {
     req.body.topic = "general";
+  } else {
+    req.body.topic = req.body.topic.toLowerCase();
   }
 
   if (req.body.message == null) {
@@ -20,11 +26,14 @@ const validateAnnoucement = (req, res, next) => {
     return res.send("Author is required");
   }
 
-  req.body.date = new Date();
+  req.body.date = new Date().toISOString();
 
   next();
 };
 
+// Routes
+
+// returns all announcements
 announcementController.get(
   "/api/announcements",
   util.logRequest,
@@ -36,31 +45,40 @@ announcementController.get(
   }
 );
 
+// returns announcement by id
 announcementController.get(
   "/api/announcements/:id",
   util.logRequest,
   async (req, res, next) => {
+
     let collection = client.db().collection("Announcements");
 
-    let announcement = await util.findOne(collection, { _id: req.params.id });
+    console.log("Requested: " + req.params.id);
+
+    let announcement = await collection.findOne({
+      _id: new ObjectId(req.params.id),
+    });
+
     res.status(200).json(announcement);
   }
 );
 
+//  returns announcement by topic
 announcementController.get(
   "/api/announcements/topic/:topic",
   util.logRequest,
   async (req, res, next) => {
+
     let collection = client.db().collection("Announcements");
 
-    let announcement = await util.findOne(collection, {
-      topic: req.params.topic,
-    });
+    console.log("Requested: " + req.params.topic);
+
+    let announcement = await util.find(collection, { topic: req.params.topic.toLowerCase() });
     res.status(200).json(announcement);
   }
 );
 
-// getByLastX (Last x announcements)
+// returns last x announcements
 announcementController.get(
   "/api/announcements/last/:count",
   util.logRequest,
@@ -76,19 +94,19 @@ announcementController.get(
   }
 );
 
-// subscribe (subscribe to a topic)
-announcementController.post(
-  "/api/announcements/subscribe/:topic",
-  util.logRequest,
-  async (req, res, next) => {
-    let collection = client.db().collection("Announcements");
+// !! TODO: subscribe (subscribe to a topic)
+// announcementController.post(
+//   "/api/announcements/subscribe/:topic",
+//   util.logRequest,
+//   async (req, res, next) => {
+//     let collection = client.db().collection("Announcements");
 
-    // TODO: Need to plan database structure for subscriptions
-    res.status(200).json(result);
-  }
-);
+//     // TODO: Need to plan database structure for subscriptions
+//     res.status(200).json(result);
+//   }
+// );
 
-// addAnnouncement
+// create announcement
 announcementController.post(
   "/api/announcements",
   validateAnnoucement,
@@ -96,26 +114,21 @@ announcementController.post(
   async (req, res, next) => {
     let collection = client.db().collection("Announcements");
 
-    let ann = new Announcement(
-      req.body.topic,
-      req.body.message,
-      req.body.author,
-      req.body.date
-    );
+    let ann = new Announcement(req.body.title, req.body.message, req.body.topic, req.body.author);
 
     let result = await util.insertOne(collection, ann);
     res.status(200).json(result);
   }
 );
 
-// deleteAnnouncement
+// delete announcement by id
 announcementController.delete(
   "/api/announcements/:id",
   util.logRequest,
   async (req, res, next) => {
     let collection = client.db().collection("Announcements");
 
-    let result = await util.deleteOne(collection, { _id: req.params.id });
+    let result = await util.deleteOne(collection, { _id: new ObjectId(req.params.id) });
     res.status(200).json(result);
   }
 );
