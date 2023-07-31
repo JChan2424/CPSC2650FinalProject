e9c300cd0b98991046eda8d71faf9b404f099e92
Joshua Chan <jchan211@mylangara.ca>
2023-07-31 11:30:10 -0700

    Removed comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 
-//import "../node_modules/bootstrap/dist/css/united.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 import "../views/css/united.min.css";
 
