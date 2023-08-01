39671fe384cbf7cf9323d4acb7a3e55a65a68cf4
Joshua Chan <jchan211@mylangara.ca>
2023-08-01 04:31:43 -0700

    Added state variables for posts. Shared so far by navbar.js and body.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Navbar from "./navbar.js";
 import Body from "./body.js";
 import Footer from "./footer.js";
-const App = props => {
 
+const App = props => {
+    const [posts, setPosts] = useState();
     return (
         <>
             <div >
-                <Navbar />
-                <Body />
+                <Navbar posts={posts} setPosts={setPosts} />
+                <Body posts={posts} setPosts={setPosts} />
                 <Footer />
             </div>
         </>
