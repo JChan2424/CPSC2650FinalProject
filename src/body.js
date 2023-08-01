36b0f176418a5874061c23a06c4b09b9bab6a832
Joshua Chan <jchan211@mylangara.ca>
2023-08-01 04:32:57 -0700

    Added attribute to pass props to posts.js.

--- src/body.js
+++ src/body.js
@@ -1,9 +1,10 @@
 import React, { useState,useEffect } from "react";
 import Weather from "./weather";
+import Posts from "./posts";
 const Body = props => {
     useEffect(() => {
         // check if credentials are in local storage
-    
+        
         if (localStorage.getItem("token") === null) {
           // if not, redirect to login page
         //   window.location.href = "/login?redirect=" + window.location.pathname;
@@ -28,19 +29,25 @@ const Body = props => {
     
           // if so, do nothing
         });
+        
+        // props.setPostArray(getMostRecentPosts());
+        // console.log(props.postArray)
       }, []);
+    // TODO: Add API request for getting get 10 most recent posts (Use effect hook)
     
+    // TODO: Add API request for getting Topics for the topics bar
     return (
         <>
             <main>
                 <div className="container text-center bg-secondary">
                     <div className="row">
                     <div className="col-2 card me-4 bg-primary">
-                        <div className="card-header bg-primary"><h3>Your Topics</h3></div>
+                        <div className="card-header bg-primary"><h3>All Topics</h3></div>
                         <div className="card-body"></div> 
                     </div>
                     <div className="col-8 card me-4">
                         <div className="card-header"><h3>Recent Posts</h3></div>
+                        <Posts posts={props.posts} setPosts={props.setPosts} />
                     </div>
                     <div className="col card bg-primary">
                         <div className="card-header bg-primary"><h3>Weather</h3></div>
