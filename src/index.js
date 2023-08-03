3e22940a1c21aa3a41391acf2249e0ae44a02958
Ashwin <69374625+xxiz@users.noreply.github.com>
2023-08-02 21:42:20 -0700

    add: example

--- src/index.js
+++ src/index.js
@@ -10,6 +10,92 @@ import Register from './Register';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Body from './body';
 
+/*
+useEffect(() => {
+    const token = localStorage.getItem("token");
+    const isPublicRoute = (path: string) => {
+      const publicRoutesRegex = [
+        /^\/$/,
+        /^\/auth\/login$/,
+        /^\/standings$/,
+        /^\/home$/,
+        /^\/404$/,
+        /^\/gallery$/,
+        /^\/about$/,
+        /^\/contact$/,
+        /^\/competitions$/,
+        /^\/contact-us$/,
+        /^\/register$/,
+        /^\/filled$/,
+        /^\/competitions\/open$/,
+        /^\/competitions\/tournaments$/,
+      ];
+
+      return publicRoutesRegex.some((regex) => regex.test(path));
+    };
+
+    const isPrivateRoute = (path: string) => {
+      const privateRoutesRegex = [
+        /^\/dashboard$/,
+        /^\/members$/,
+        /^\/members\/create$/,
+        /^\/members\/update\/\d+$/,
+        /^\/memberships$/,
+        /^\/memberships\/create$/,
+        /^\/memberships\/update\/\d+$/,
+        /^\/events$/,
+        /^\/events\/update\/\d+$/,
+        /^\/events\/create$/,
+        /^\/events\/create-category$/,
+        /^\/transactions\/\d+$/,
+        /^\/users$/,
+        /^\/users\/update\/\d+$/,
+        /^\/users\/create$/,
+        /^\/teams$/,
+        /^\/teams\/update\/\d+$/,
+        /^\/teams\/members$/,
+        /^\/teams\/members\/update\/\d+$/,
+        /^\/matches$/,
+        /^\/matches\/match\/\d+$/,
+        /^\/matches\/create\/result$/,
+        /^\/matches\/create\/score$/,
+        /^\/events\/categories$/,
+        /^\/events\/categories\/update\/\d+$/,
+        /^\/events\/upcoming$/,
+        /^\/events\/upcoming\/\d+$/,
+        /^\/user\/profile\/\d+$/,
+        /^\/announcements$/,
+        /^\/dashboard\/inquiry\/\d+$/,
+
+        // Logged in member routes
+        /^\/member\/events$/,
+      ];
+
+      return privateRoutesRegex.some((regex) => regex.test(path));
+    };
+
+    if (isPublicRoute(location.pathname)) {
+      // Public route, do nothing
+    } else if (isPrivateRoute(location.pathname)) {
+      if (!token) {
+        showNotification({
+          title: "Error",
+          message: "You are not authorized to view this page.",
+          color: "red",
+          icon: <IconExclamationMark />,
+          autoClose: true,
+        });
+        navigate("/auth/login");
+        setAuthenticated(false);
+        localStorage.clear();
+      }
+    } else {
+      // If the route is not found in publicRoutes or privateRoutes, redirect to 404
+      navigate("/404");
+    }
+  }, [location]);
+*/
+
 const router = createBrowserRouter([
     {
         path: "/",
