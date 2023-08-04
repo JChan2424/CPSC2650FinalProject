e16191212255d643f16c82ddc2658e3dbcab5778
Joshua Chan <jchan211@mylangara.ca>
2023-08-04 04:36:30 -0700

    Added API key to config file.

--- models/config/config.js
+++ models/config/config.js
@@ -4,4 +4,5 @@ config.SERVER = 'myfirstcluster.c8cef9g.mongodb.net'
 config.USERNAME = '2650_FinalProj_User'
 config.PASSOWRD = '1234'
 config.DATABASE = '2650_FinalProject'
+config.API_KEY = '4dd47ed21e20bd1fa1893e2502ce3ef0'
 module.exports = config
