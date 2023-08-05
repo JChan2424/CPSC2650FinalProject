0945200d1c852b7745767b06663c8c9891bfd8e0
Ashwin <69374625+xxiz@users.noreply.github.com>
2023-08-04 20:22:17 -0700

    add: info about config

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -1,6 +1,6 @@
 const config = {}
-config.SECRET = 'hentai'
-config.INVITE = 'bruh'
-config.SESSION_LENGTH = '6h'
+config.SECRET = 'hentai' // The secret key used to sign JWT tokens
+config.INVITE = 'bruh' // The invite code to create an account with elevated permissions
+config.SESSION_LENGTH = '6h' // How long a login session lasts
 
 module.exports = config
