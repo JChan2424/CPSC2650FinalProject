cb8ab4b52346c1a87b36cb7c0e5ff64d503ff58b
Ashwin <69374625+xxiz@users.noreply.github.com>
2023-08-04 20:21:07 -0700

    add: imrpove readability & organize

--- controllers/utilController.js
+++ controllers/utilController.js
@@ -1,12 +1,20 @@
+// Import dependencies
 const util = require('../models/util.js');
 const express = require('express');
+
+// Retrieve MongoDB client
 const client = util.getMongoClient();
+
+// Create express router
 const utilController = express.Router();
 
 utilController.get("/api/search/:term", util.logRequest, async (req, res, next) => {
 
+    // Get the "Announcements" collection
     let collection = client.db().collection("Announcements");
 
+
+    // Search the collection
     let results = [];
 
     await collection.find({}).forEach((entry) => {
@@ -19,6 +27,7 @@ utilController.get("/api/search/:term", util.logRequest, async (req, res, next) 
         }
     });
 
+    // Respond with the search results
     res.status(200).json({
         success: true,
         data: results
