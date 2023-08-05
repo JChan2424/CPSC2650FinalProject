32e0294734a7cf96a07ccde6b8bd10131b54920a
Ashwin <69374625+xxiz@users.noreply.github.com>
2023-08-04 20:19:05 -0700

    add: improve readability

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,133 +1,145 @@
-const util = require("../models/util.js");
-const config = require("./config/config.js");
-const client = util.getMongoClient();
-const User = require("../models/user");
-const jwt = require("jsonwebtoken");
+// Import dependencies
 const express = require("express");
-const authController = express.Router();
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
-// TODO: Fix Roles
+// Import custom modules and configuration
+const config = require("./config/config.js");
+const util = require("../models/util.js");
+const User = require("../models/user");
+
+// Initialize MongoDB client and Express Router
+const client = util.getMongoClient();
+const authController = express.Router();
+
+// Middleware to parse urlencoded bodies and JSON bodies
 authController.use(express.urlencoded({ extended: true }));
 authController.use(express.json());
-authController.post("/api/register", util.logRequest, async (req, res) => {
-    const { username, password, confirmedPassword, invite, role } = req.body;
 
+// Helper function to validate user input
+const verifyInput = (req, res, confirmPassword = false) => {
+    const { username, password, confirmedPassword } = req.body;
+
+    // Check if username and password are provided
     if (!username || !password) {
         res.status(400);
-        return res.json({
+        return {
             success: false,
             message: "Username and password are required",
-        });
+        };
     }
 
-    if (password != confirmedPassword) {
+    // If confirmation of password is required, check if passwords match
+    if (confirmPassword && password != confirmedPassword) {
         res.status(400);
-        return res.json({ success: false, message: "Passwords do not match" });
+        return { success: false, message: "Passwords do not match" };
     }
 
+    return null;
+};
+
+// Route to register a new user
+authController.post("/api/register", util.logRequest, async (req, res) => {
+    // Validate input
+    let response = verifyInput(req, res, true);
+    if (response) return res.json(response);
+
+    const { username, password, invite, role } = req.body;
     let collection = client.db().collection("Users");
 
+    // Check if username already exists
     let result = await collection.findOne({ username: username });
-
     if (result != null) {
         res.status(409);
         return res.json({ success: false, message: "Username already exists" });
     }
 
-    hashedPassword = crypto.createHash("sha1").update(password).digest("hex");
-    console.log("Password: " + hashedPassword);
+    // Hash password for secure storage
+    let hashedPassword = crypto
+        .createHash("sha1")
+        .update(password)
+        .digest("hex");
 
-    let user;
+    // Create new user instance
+    let user =
+        invite == config.INVITE
+            ? new User(username, hashedPassword, role)
+            : new User(username, hashedPassword, "USER");
 
-    if (invite == config.INVITE) {
-        user = new User(username, hashedPassword, role);
-    } else {
-        user = new User(username, hashedPassword, "USER");
-    }
-
-    
-
+    // Attempt to insert the new user into the database
     try {
         await util.insertOne(collection, user);
-
-        return res.status(200).json({
-            success: true,
-            message: "User created",
-        });
+        return res.status(200).json({ success: true, message: "User created" });
     } catch (err) {
+        // In case of error, return a failure message
         res.status(500);
         return res.json({ success: false, message: "Failed to create user" });
     }
 });
 
+// Route to login a user
 authController.post("/api/login", util.logRequest, async (req, res) => {
+    // Validate input
+    let response = verifyInput(req, res);
+    if (response) return res.json(response);
+
     const { username, password } = req.body;
+    let hashedPassword = crypto
+        .createHash("sha1")
+        .update(password)
+        .digest("hex");
 
-    // Check if username and password are provided
-    if (!username || !password) {
-        return res
-            .status("402")
-            .json({
-                success: false,
-                message: "Username and password are required",
-            });
-    }
-    hashedPassword = crypto.createHash("sha1").update(password).digest("hex");
-
+    // Attempt to find user in the database
     let collection = client.db().collection("Users");
-
     let result = await collection.findOne({
         username: username,
         password: hashedPassword,
     });
 
+    // If user not found, return a failure message
     if (result == null) {
         return res
             .status(401)
             .json({ success: false, message: "Invalid credentials" });
     }
 
+    // If user found, proceed with generating a token
     let role = result.role;
-
     try {
         const token = jwt.sign(
             { username: username, role: role },
             config.SECRET,
-            {
-                expiresIn: config.SESSION_LENGTH,
-            }
+            { expiresIn: config.SESSION_LENGTH }
         );
-
         res.status(200).json({
             success: true,
             token: token,
             role: role,
             username: username,
-            role: role,
         });
     } catch (err) {
+        // In case of error, return a failure message
         res.status(500);
         return res.json({ success: false, message: "Failed to create token" });
     }
 });
 
+// Route to verify user token
 authController.post("/api/verify", util.logRequest, (req, res) => {
-    console.log("inside veerify", req.body);
-    // Check if token is provided and if so get the token
     const token =
         req.headers.authorization && req.headers.authorization.split(" ")[1];
 
-    // If token is not provided return 403
+    // If no token provided, return a failure message
     if (!token) {
         return res
             .status(403)
             .json({ success: false, message: "No token provided" });
     }
 
+    // Attempt to verify the token
     jwt.verify(token, config.SECRET, (err, decoded) => {
-        // If token is invalid return 401
         if (err) {
+            // If token is invalid, return a failure message
             return res
                 .status(401)
                 .json({
@@ -135,8 +147,7 @@ authController.post("/api/verify", util.logRequest, (req, res) => {
                     message: "Failed to authenticate token",
                 });
         }
-
-        // If token is valid return 200
+        // If token is valid, return success message with username and role
         return res.json({
             success: true,
             username: decoded.username,
@@ -145,4 +156,5 @@ authController.post("/api/verify", util.logRequest, (req, res) => {
     });
 });
 
+// Export the authController for use in other modules
 module.exports = authController;
